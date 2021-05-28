7ef33471fb08a5e260dd9334cbba87fc65e8cbfd
I Gede Bagus Wirawan <gusdechang24@gmail.com>
2021-05-28 23:34:38 +0800

    Upgrade Project

---
+++ dashboard.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Nusanta Art</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	
+	<!-- CSS -->
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<!-- Header -->
+	<nav class="navbar">
+		<header>
+			<div class="logo">
+				<img src="img/nusanta-art.png">
+			</div>
+			<div>
+				<input type="text" placeholder="Search.." name="find">
+			</div>
+			<div>
+				<span>
+					<a href="#">Login</a>
+				</span>
+			</div>
+		</header>
+	<!-- End of Header -->
+
+	<!-- main -->
+	<div class="main">
+		<!-- Floating Menu -->
+		<div class="Floating-menu">
+			<ul>
+				<li><a href="#">Dashboard</a></li>
+				<li><a href="#">Profile</a></li>	
+				<li><a href="#">Help</a></li>	
+				<li><a href="#">About</a></li>
+			</ul>
+			<div class="logo-in-menu">
+				<img src="img/nusanta-art.png">
+			</div>
+		</div>
+		<!-- End of Floating Menu -->
+
+		<!-- The Content -->
+		<div class="content">
+			<!-- sliding content -->
+			<div class="sliding-content">
+				<p>TRANDING</p>
+				<div class="img-slide">
+
+				</div>
+				<div class="slide-button">
+					<span class="button-circle"></span>
+				</div>
+				<!-- end of sliding content -->
+
+				<!-- Stories -->
+				<div class="story">
+					<div class="img-story">
+						<div class="floating-profile">
+							<p>pict here</p>
+						</div>
+					</div>
+					<div class="img-story">
+						<div class="floating-profile">
+							<p>pict here</p>
+						</div>
+					</div>
+					<div class="img-story">
+						<div class="floating-profile">
+							<p>pict here</p>
+						</div>
+					</div>
+						<div class="img-story">
+							<p>pict here</p>
+						<div class="floating-profile">
+							<p>pict here</p>
+						</div>
+					</div>
+				</div>
+				<!-- End of Stories -->
+
+				<!-- Video list -->
+				<div class="video-list">
+					<div class="content-box">
+						<div class="video-box">
+							
+						</div>
+						<a class="nama-video" href=""></a>
+						<div class="profile-in-video">
+							
+						</div>
+					</div>
+				</div>
+				<!-- End of Video list -->
+
+			</div>
+
+		</div>
+	</div>
+	<!-- End of Main -->
+	</nav>
+</body>
+</html>
